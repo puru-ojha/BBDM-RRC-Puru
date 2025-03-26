fa8fe4bbd889a2ea50302952311b9a87b7842b64
AniruthSuresh <aniruthzlatan@gmail.com>
2025-03-27 02:11:34 +0530

    fix : changed the bash script to account for conda env

--- train.sh
+++ train.sh
@@ -5,7 +5,7 @@
 #SBATCH --partition=lovelace
 #SBATCH --time=3-00:00:00
 
-
+conda init
 conda activate BBDM
 
 python3 main.py --config /home2/aniruth.suresh/BBDM-RRC/configs/Template-LBBDM-f4.yaml --train --sample_at_start --save_top --gpu_ids 0 
