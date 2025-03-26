eb2171308bcacee63615f9391e0a5e7d237e0409
AniruthSuresh <aniruthzlatan@gmail.com>
2025-03-27 02:04:00 +0530

    chore : added bash training script

--- train.sh
+++ train.sh
@@ -1,2 +1,13 @@
-python3 main.py --config /home/aniruth/Desktop/RRC/BBDM-RRC/configs/Template-LBBDM-f4.yaml --train --sample_at_start --save_top --gpu_ids 0 
+#!/bin/bash
+#SBATCH -A kcis
+#SBATCH -n 10
+#SBATCH --gres=gpu:1
+#SBATCH --partition=lovelace
+#SBATCH --time=3-00:00:00
+
+
+conda activate BBDM
+
+python3 main.py --config /home2/aniruth.suresh/BBDM-RRC/configs/Template-LBBDM-f4.yaml --train --sample_at_start --save_top --gpu_ids 0 
+
 
