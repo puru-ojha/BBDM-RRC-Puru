3cb22822ff52164824687d81790729edbef36710
AniruthSuresh <aniruthzlatan@gmail.com>
2025-03-27 14:01:29 +0530

    chore : added test script

--- train.sh
+++ train.sh
@@ -10,6 +10,4 @@ conda activate BBDM
 
 python3 main.py --config /home2/aniruth.suresh/BBDM-RRC/configs/Template-LBBDM-f4.yaml --train --sample_at_start --save_top --gpu_ids 0 
 
-python3 main.py --config /home2/aniruth.suresh/BBDM-RRC/configs/Template-LBBDM-f4.yaml --sample_to_eval --gpu_ids 0 --resume_model /home2/aniruth.suresh/BBDM-RRC/results/xarm2panda/LBBDM-f4/checkpoint/top_model_epoch_78.pth
-
 
